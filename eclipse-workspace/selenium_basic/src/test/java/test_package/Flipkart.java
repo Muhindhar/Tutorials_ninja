239d62542baeaba6b290c61cafd120e30f116493
Muhindhar <muhindharsv27@gmail.com>
2026-04-01 11:29:11 +0530

    updated

--- eclipse-workspace/selenium_basic/src/test/java/test_package/Flipkart.java
+++ eclipse-workspace/selenium_basic/src/test/java/test_package/Flipkart.java
@@ -1,33 +1,29 @@
 package test_package;
-import org.openqa.selenium.*;
-import org.openqa.*;
-import org.openqa.selenium.chrome.*;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Flipkart {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        ChromeOptions options = new ChromeOptions();
+        WebDriver driver = new ChromeDriver(options);
 
-			WebDriver driver = new ChromeDriver();
-			driver.get("https://www.flipkart.com/");
+        driver.get("https://www.flipkart.com/");
 
-			String title = driver.getTitle();
-			System.out.println(title);
-			System.out.println("Title Length : "+title.length());
-			
-			
-			String url = driver.getCurrentUrl();
-			String exp = "https://www.flipkart.com/";
-			
-			if (exp.equals(url)) {
-				System.out.println("it is correct URL");
-			}else {
-				System.out.println("not a correct URL");
-			}
-
-			
-			String page = driver.getPageSource();
-			
-			System.out.println("PAGE LENGTH : "+page.length());
-			driver.close();
-		}
+        String title = driver.getTitle();
+        System.out.println(title);
+        System.out.println("Title Length : " + title.length());
+        String url = driver.getCurrentUrl();
+        String exp = "https://www.flipkart.com/";
+        if (exp.equals(url)) {
+            System.out.println("It is correct URL");
+        } else {
+            System.out.println("Not a correct URL");
+        }
+        String page = driver.getPageSource();
+        System.out.println("PAGE LENGTH : " + page.length());
+        driver.quit();
+    }
 }
