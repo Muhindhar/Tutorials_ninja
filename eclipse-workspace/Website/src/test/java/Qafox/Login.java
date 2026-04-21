e356c794e22b9b157793ee947a681d2f04520fb3
Muhindhar <muhindharsv27@gmail.com>
2026-04-21 22:24:08 +0530

    Update Login.java

--- eclipse-workspace/Website/src/test/java/Qafox/Login.java
+++ eclipse-workspace/Website/src/test/java/Qafox/Login.java
@@ -23,7 +23,13 @@ public class Login {
     @Test(dataProvider = "validData", dataProviderClass = com.utilities.ExcelData.class)
     public void validLoginTest(String email, String password) {
 
-        driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+       WebElement myAccount = wait.until(
+    ExpectedConditions.elementToBeClickable(
+        By.xpath("//span[normalize-space()='My Account']")));
+
+((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", myAccount);
+
+myAccount.click();
         driver.findElement(By.xpath("//a[text()='Login']")).click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-email")));
